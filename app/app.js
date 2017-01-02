db8691ffec6cb190083260175b71d215918955cd
nihit kasabwala <nihit kasabwala>
2017-01-01 19:27:10 -0500

    initial commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var _appodule = angular.module('DemoRestaurantApp', [
+	'ngRoute']);
+
+_appodule.config(function ($routeProvider) {
+	$routeProvider
+		.when ('/home', {
+			templateUrl: '',
+			controler: ''
+		})
+});
